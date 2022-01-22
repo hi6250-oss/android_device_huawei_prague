3c2b7592fa71fa9f8db181e54fa6da8aaa54a3a4
A2L5E0X1 <alexed25@protonmail.com>
2022-01-22 15:34:15 +0100

    prague: configs: drop second Android.mk
    * not needed to have seperated makefiles for every config folder
    
    Change-Id: Ic6d91e474445ce1775ad6ed48699d65648f6bc84

--- configs/Android.mk
+++ configs/Android.mk
@@ -6,4 +6,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+#
+# init
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.hisi.odm.rc
+LOCAL_SRC_FILES := init/init.hisi.odm.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ODM_ETC)/init
+include $(BUILD_PREBUILT)
